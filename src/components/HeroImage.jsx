24ed2a641414811f8a033671fe494c73e3513794
Mario Lisbona <mario.lisbona@gmail.com>
2023-01-19 21:03:31 +1100

    revert to original, need to reserach. working locally

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -4,8 +4,7 @@ import './HeroImage.css'
 const HeroImage = ({ heroClass }) => {
 	return (
 		<>
-			{/* <section className={heroClass}> */}
-			<section className="test" style ={ { background: "url('../src/assets/hero-img-landing.jpg')" } }>
+			<section className={heroClass}>
         <span className="background-image" role="img" aria-label="Mountain range in Austria"></span>
       </section>
 		</>
