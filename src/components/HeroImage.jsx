8b29a727615a69050d100f39c2af127b0dd3ab02
Mario Lisbona <mario.lisbona@gmail.com>
2023-01-19 20:57:28 +1100

    Test for inline style for background hero img

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -4,8 +4,8 @@ import './HeroImage.css'
 const HeroImage = ({ heroClass }) => {
 	return (
 		<>
-			{/* <section className="hero-image"> */}
-			<section className={heroClass}>
+			{/* <section className={heroClass}> */}
+			<section className="test" style ={ { background: "url('../src/assets/hero-img-landing.jpg')" } }>
         <span className="background-image" role="img" aria-label="Mountain range in Austria"></span>
       </section>
 		</>
