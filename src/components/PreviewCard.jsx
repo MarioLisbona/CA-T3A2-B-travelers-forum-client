fc7a54766c4146f49bf3d983d1eb122fcaf364d8
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-24 16:05:13 +1100

    Author and data for preview cards

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -16,14 +16,16 @@ import { Link, useNavigate } from 'react-router-dom'
 
 const PreviewCard = ({ post }) => {
 
-
+	console.log(post.title.length)
 	return (
 		<>
 			<div className="col-md-6 col-lg-4 col-xl-3">
 				<div className="card mx-auto m-1 bg-secondary text-white">
 						<div className="card-body">
 								<h5 className="card-title">{post.title}</h5>
-								<h6>{`Author: ${post.author}`}</h6>
+								<h6>{`Author: ${post.author.username}`}</h6>
+								<h6>{`Author: ${post.date_posted.substring(0, 10)}`}</h6>
+								<hr></hr>
 								<p className="card-text">{`${post.content.substring(0, 150)}......`}</p>
 								<Link to={`/posts/${post._id}`} className="btn btn-primary">
 									Read more
