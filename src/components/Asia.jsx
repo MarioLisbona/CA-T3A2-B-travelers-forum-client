e8e6a7763b22ea0cdfc89ba7d9c72a8fcf79fbd9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-25 22:37:11 +1100

    Clean up ternery's in asia and comment

--- src/components/Asia.jsx
+++ src/components/Asia.jsx
@@ -25,26 +25,46 @@ import MemberNavBar from './MemberNavBar'
 
 const Asia = ({ forumMember, asiaPosts }) => {
 
+  const emptyPostsArray = []
   return (
     <>
+      {/* conditionally render navbar for guest and member */}
       {forumMember ? <MemberNavBar /> : <NavBar />}
+      {/* Heroimage */}
       <HeroImage heroClass={'asia'}/>
+        {/* container for main body of the page */}
         <div className="container">
           <Header headingText={"Welcome to the Asian Continent!"}
             bodyText={"This is a little blurb about the Asian content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, q...... "}
           />
           <div className="row g-3">
-            {asiaPosts.length > 0
+            {/* If asiaPosts has objects then map over the array and render a preview card for each post
+            and pass in the post object */}
+            {emptyPostsArray.length > 0
               ? asiaPosts.map((post, idx) => (
                 <PreviewCard key ={idx} post={post}  />
               ))
-              : forumMember
-                  ? <Header headingText={"Much empty..."} 
-                    bodyText={"The Asia travel forum currently has no posts, Click below to be the first"} 
-                    showBtn1 btn1Text={"Add a post"} btn1ToPage={"/posts/create"} />
-                  : <Header headingText={"Much empty..."} 
-                  bodyText={"The Asia travel forum currently has no posts, register today to be the first to contribute."}
-                    showBtn1 btn1Text={"Register"} btn1ToPage={"/register"} />
+              // If asiaPosts is an empty array then render a message for guest and one for member
+              // guest message will prompt to register to make a post with CTA button
+              // member message will prompt to make a post with CTA button
+              : <Header headingText={"Much empty..."}
+                bodyText={
+                  forumMember
+                  ? "The Asia travel forum currently has no posts, Click below to be the first"
+                  : "The Asia travel forum currently has no posts, register today to be the first to contribute."
+                }
+                showBtn1
+                btn1Text={
+                  forumMember
+                    ? "Add a post"
+                    : "Register"
+                }
+                btn1ToPage={
+                  forumMember
+                    ? "/posts/create"
+                    : "/register"
+                }
+              />
             }
           </div>
         </div>
