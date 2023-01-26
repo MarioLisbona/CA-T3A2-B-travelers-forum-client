e27867b913aa4eac80a3b445f123f77b2548f6d9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-26 21:10:56 +1100

    filter my posts for myposts page

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -3,21 +3,27 @@ import HeroImage from './HeroImage'
 import PreviewCard from './PreviewCard'
 import MemberHeader from './MemberHeader'
 
-const MyPosts = ({ latestPosts }) => {
+const MyPosts = ({ latestPosts, loggedInMember }) => {
+
+  console.log(latestPosts[0].author._id)
+  console.log(loggedInMember.id)
+
+  const myPosts = latestPosts.filter(post => post.author._id == loggedInMember.id)
+  console.log(myPosts)
 
   return (
     <>
       <HeroImage heroClass={'landing-page'}/>
       <div className="container">
-      <MemberHeader headingText={`Hi SOME PERSONS NAME, welcome to the forum`}
-        bodyText={"All your latest posts are displayed below"} 
-        btn1Text={'View All My Posts'}
-        btn2Text={'Create a Post'}
-        showBtn1  showBtn2
+      <MemberHeader headingText={`Hi ${loggedInMember.username}, welcome to the forum`}
+        bodyText={"All your posts are displayed below"} 
+        btn1Text={'Create a Post'}
+        showBtn1
+        btn1ToPage={'/create/'}
       />
       <div className="row g-3">
-        {latestPosts.length > 0
-          ? latestPosts.map((post, idx) => (
+        {myPosts.length > 0
+          ? myPosts.map((post, idx) => (
               <PreviewCard key ={idx} post={post}  />
             ))
           : <MemberHeader headingText={"You havn't made any posts yet."}
