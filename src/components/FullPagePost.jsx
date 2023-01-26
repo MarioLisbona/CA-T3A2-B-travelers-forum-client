1f1c4a8d98b3c0d6d97cfcfd20462910816af0f0
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-26 18:21:00 +1100

    message for no comments on post not logged in

--- src/components/FullPagePost.jsx
+++ src/components/FullPagePost.jsx
@@ -47,27 +47,15 @@ const FullPagePost = ({ forumMember, post, submitComment }) => {
           ? comments.map((comment, idx) => (
               <CommentContent key={idx} comment={comment}/>
             ))
-          : <Header headingText={"Much empty..."}
-              bodyText={
-                forumMember
-                  ? "This post currently has no comments. Comment Below below to be the first"
-                  : "This post currently has no comments. Register today to be the first to comment."
-              }
-              btn1Text={
-                forumMember
-                  ? ""
-                  : "Register"
-              }
-              showBtn1={
-                forumMember
-                  ? false
-                  : true
-              }
-              btn1ToPage={
-                forumMember
-                ? "/posts/create"
-                : "/register" }
-            />
+            : <Header
+                bodyText={'Only Members can comment on a post. Register today to become a member or login'}
+                showBtn1 
+                btn1Text={"Register"}
+                btn1ToPage={"/register"}
+                showBtn2
+                btn2Text={"Login"}
+                btn2ToPage={"/login"}
+              />
         }
         {comments.length > 0 ? <h3 className="ps-5 my-3">Post a Comment</h3> : ''}
         {forumMember
@@ -76,7 +64,7 @@ const FullPagePost = ({ forumMember, post, submitComment }) => {
           : comments.length == 0 
             ? ''
             : <Header
-                bodyText={'Only Members can comment on a post. Register today to become a member or sign in'}
+                bodyText={'Only Members can comment on a post. Register today to become a member or login'}
                 showBtn1 
                 btn1Text={"Register"}
                 btn1ToPage={"/register"}
