180296ccecf4f13fc081d14cc0f11d71cfcb1f26
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-26 19:13:16 +1100

    clean up styling on PreviewCard

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -6,7 +6,7 @@ const PreviewCard = ({ post }) => {
 	return (
 		<>
 			<div className="col-md-6 col-lg-4 col-xl-3">
-				<div className="card mx-auto m-1 bg-secondary text-white border border-success">
+				<div className="card mx-auto m-1 bg-secondary text-white border border-success" style={{ minHeight: "350px", maxHeight: "500px"}}>
 						<div className="card-body">
 								<h5 className="card-title">{post.title.substring(0, 100)}</h5>
 								{/* need to wait till structured of posted posts is correct in DB */}
@@ -14,7 +14,8 @@ const PreviewCard = ({ post }) => {
 								<h6>{`Author: ${post.date_posted.substring(0, 10)}`}</h6>
 								<hr></hr>
 								<p className="card-text">{`${post.content.substring(0, 150)}......`}</p>
-								<Link to={`/posts/${post._id}`} className="btn btn-primary">
+								
+								<Link to={`/posts/${post._id}`} className="btn btn-primary position-absolute bottom-0 start-10 translate-x my-3">
 									Read more
 								</Link>
 						</div>
