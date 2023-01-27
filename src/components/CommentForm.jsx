bee1ccc72dcddf0636e25b3e0c07f8083eceea11
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-27 20:07:08 +1100

    remove title and continent from comment form

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -17,18 +17,6 @@ const CommentForm = ({ post, submitComment }) => {
   return (
     <>
     <form onSubmit={submitForm} className="p-5 bg-light rounded-3 border border-success">
-      <div className="row mb-3">
-        <label className="col-sm-2 col-form-label">Post Title</label>
-        <div className="col-sm-10">
-          <input type="text" value={title} className="form-control" id="title"></input>
-        </div>
-      </div>
-      <div className="row mb-3">
-        <label className="col-sm-2 col-form-label">Continent</label>
-        <div className="col-sm-10">
-          <input type="text" value={continent} className="form-control" id="continent"></input>
-        </div>
-      </div>
       <fieldset className="row mb-3">
         <legend className="col-form-label col-sm-2 pt-0">Comment</legend>
         <div className="col-sm-10">
