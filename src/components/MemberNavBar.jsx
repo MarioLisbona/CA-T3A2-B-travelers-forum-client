cedd2f7c0826bfdd51e1bee3437897605f1ff727
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-01-30 21:14:33 +1100

    remove Home from navbars

--- src/components/MemberNavBar.jsx
+++ src/components/MemberNavBar.jsx
@@ -20,11 +20,6 @@ const MemberNavBar = ({ logoutMember, loggedInMember }) => {
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul className="navbar-nav mr-auto">
-                      <li className="nav-item active">
-                          <Link className="nav-link text-success-emphasis" to="/">
-                              Home
-                          </Link>
-                      </li>
                       <li className="nav-item">
                           <Link className="nav-link text-white" to="../view/all">
                               Browse all Destinations
