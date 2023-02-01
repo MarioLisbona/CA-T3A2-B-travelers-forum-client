09b0b8d3ed441bd89bc5e08b473ce843cce86901
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-02-01 13:28:59 +1100

    clean up attributes in Post Modal

--- src/components/ModalPost.jsx
+++ src/components/ModalPost.jsx
@@ -15,24 +15,44 @@ const Modal = ({ modalNumber, post, editPost }) => {
   // display modal for editing a post
   return (
     <>
-      <div className="modal fade" data-keyboard="false" data-backdrop="static" id={`modal${modalNumber}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div className="modal fade" data-keyboard="false" data-backdrop="static" 
+        id={`modal${modalNumber}`} 
+        tabIndex="-1" role="dialog" 
+        aria-labelledby="exampleModalCenterTitle" 
+        aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered" role="document">
           <div className="modal-content p-3">
             <div className="modal-header p-0 py-3">
               <h5 className="modal-title" id="exampleModalLongTitle">Edit your Post</h5>
               {/* close modal without saving changes */}
-              <button type="button" className="close btn btn-success" data-dismiss="modal" aria-label="Close">
+              <button 
+                type="button" 
+                className="close btn btn-success" 
+                data-dismiss="modal" 
+                aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
               <div class="form-group">
                 <label for="recipient-name" class="col-form-label">Title</label>
-                <input required maxLength="50" type="text" value={title} onChange={(event) => setTitle(event.target.value)} className="form-control" id="title"></input>
+                <input 
+                  required maxLength="50" 
+                  type="text" 
+                  value={title} 
+                  onChange={(event) => setTitle(event.target.value)} 
+                  className="form-control" 
+                  id="title">
+                </input>
               </div>
               <label for="inputPostContinent" class="col-form-label">Continent</label>
               <div class="form-group">
-                <select required className="form-select" value={continent} onChange={(event) => setContinent(event.target.value)} id="inputPostContinent">
+                <select 
+                  required 
+                  className="form-select" 
+                  value={continent} 
+                  onChange={(event) => setContinent(event.target.value)} 
+                  id="inputPostContinent">
                   <option selected></option>
                   <option value="Asia">Asia</option>
                   <option value="Africa">Africa</option>
@@ -45,12 +65,24 @@ const Modal = ({ modalNumber, post, editPost }) => {
               </div>
               <div class="form-group">
                 <label for="message-text" class="col-form-label">Post</label>
-                <textarea required maxLength="10000" className="form-control" rows="15" value={postContent} onChange={(event) => setPostContent(event.target.value)}></textarea>
+                <textarea 
+                  required 
+                  maxLength="10000" 
+                  className="form-control" 
+                  rows="15" 
+                  value={postContent} 
+                  onChange={(event) => setPostContent(event.target.value)}>
+                </textarea>
               </div>
             </div>
             <div className="modal-footer p-0 py-3">
               {/* save changed by calling editPostModal and close modal */}
-              <button type="button" onClick={() => {editPostModal()}} className="btn btn-success" data-dismiss="modal">Save changes</button>
+              <button 
+                type="button" 
+                onClick={() => {editPostModal()}} 
+                className="btn btn-success" 
+                data-dismiss="modal">Save changes
+              </button>
             </div>
           </div>
         </div>
