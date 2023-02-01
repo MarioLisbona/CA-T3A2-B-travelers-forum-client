05f36e2e3ca0bea4c9881f1c0028045a00c8f380
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-02-01 13:14:38 +1100

    remove link from commentContent

--- src/components/CommentContent.jsx
+++ src/components/CommentContent.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import Modal from './Modal'
 import ModalConfirmDelete from './ModalConfirmDelete'
 import moment from 'moment'
