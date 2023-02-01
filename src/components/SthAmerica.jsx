7ea5904cc422ec4b820c53f9011d6f0d3c7fb92b
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-02-01 13:38:17 +1100

    Remove Footer import from South America

--- src/components/SthAmerica.jsx
+++ src/components/SthAmerica.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import HeroImage from './HeroImage'
 import Header from './Header'
 import PreviewCard from './PreviewCard'
-import Footer from './Footer'
 
 const SthAmerica = ({ forumMember, sthAmericaPosts }) => {
 
