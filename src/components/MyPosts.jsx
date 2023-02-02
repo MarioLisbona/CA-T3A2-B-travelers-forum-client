0d8211a5fd6deb3c87d4e01cdf4f3ae765d90caa
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-02-02 11:15:03 +1100

    update message on /posts for guest and member

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import HeroImage from './HeroImage'
 import PreviewCard from './PreviewCard'
 import Header from './Header'
+import MemberHeader from './MemberHeader'
 
-const MyPosts = ({ latestPosts, loggedInMember }) => {
+const MyPosts = ({ latestPosts, loggedInMember, forumMember }) => {
 
 // filter all posts to just ones created by the member logged in
   const myPosts = latestPosts.filter(post => post.author._id == loggedInMember.id)
@@ -13,23 +14,54 @@ const MyPosts = ({ latestPosts, loggedInMember }) => {
       {/* rednering a page with only 8 preview cards */}
       <HeroImage heroClass={'landing-page'}/>
       <div className="container">
-      <Header headingText={typeof loggedInMember.username === 'undefined' ? "Welcome to the Traveler's Forum" : `Hi ${loggedInMember.username}, welcome to the forum`}
-        bodyText={"All your posts are displayed below"} 
-        btn1Text={'Create a Post'}
-        showBtn1
-        btn1ToPage={'/posts/new'}
-      />
-      <div className="row g-3">
-        {myPosts.length > 0
-          ? myPosts.map((post, idx) => (
-              <PreviewCard key ={idx} post={post} editDelete  />
-            ))
-          : <Header headingText={"You havn't made any posts yet."}
-              bodyText={"Click the Create a Post button above to make your first post to the forum."} 
-            />
-        }
-      </div>
-          
+        {/* <Header 
+          headingText={
+            typeof loggedInMember.username === 'undefined'
+              ? "Welcome to the Traveler's Forum" 
+              : `Hi ${loggedInMember.username}, welcome to the forum`
+          }
+          bodyText={"All your posts are displayed below"} 
+          btn1Text={'Create a Post'}
+          showBtn1
+          btn1ToPage={'/posts/new'}
+        /> */}
+        {forumMember 
+            ? <MemberHeader headingText={`Hi ${loggedInMember.username}, welcome to the forum`}
+                bodyText={"A blurb about how cool the forum is etc"} 
+                btn1Text={'My Posts'}
+                btn2Text={'Create a Post'}
+                showBtn1
+                showBtn2
+              />
+            : <Header headingText={"Welcome to the Traveler's Forum"}
+                bodyText={"A blurb about how cool the forum is etc and prompt the guest to register to receive member benefits or just login to use the forum"} 
+                btn1Text={'Register'}
+                showBtn1
+                btn1ToPage={"/register"}
+                btn2Text={'Login'}
+                showBtn2
+                btn2ToPage={"/login"}
+              />
+          }
+        <div className="row g-3">
+          {myPosts.length > 0
+            ? myPosts.map((post, idx) => (
+                <PreviewCard key ={idx} post={post} editDelete  />
+              ))
+            : <Header 
+                headingText={
+                  forumMember 
+                    ? "You havn't made any posts yet." 
+                    : "Sign up or Login"
+                }
+                bodyText={
+                  forumMember 
+                    ? "Click the Create a Post button above to make your first post to the forum."
+                    : "Click the Login or Register button above to become a use the forum"
+                } 
+              />
+          }
+        </div>
       </div>
     </>
   )
