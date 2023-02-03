310f7c29ca174e181a339f94855529b124b0bcce
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-02-03 14:01:01 +1100

    PreviewCard test Complete

--- tests/PreviewCard.test.jsx
+++ tests/PreviewCard.test.jsx
@@ -5,15 +5,14 @@ import { BrowserRouter, Link } from "react-router-dom"
 import App from "../src/App"
 import LandingPage from "../src/components/LandingPage"
 import { expect } from "vitest"
-import Header from '../src/components/Header'
-import Login from '../src/components/Login'
-import NavBar from '../src/components/NavBar'
-import LoginForm from '../src/components/LoginForm'
 import PreviewCard from '../src/components/PreviewCard'
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import moment from 'moment/moment'
 
 
 describe('PreviewCard Component', () => {
-  let container
 
   let testPost = {
     "_id": "63dae1618c42e092ac5b80b0",
@@ -29,22 +28,75 @@ describe('PreviewCard Component', () => {
     "__v": 0
   }
 
-  beforeEach( function () {
-    container = render(
-      <BrowserRouter>
-        <App>
-          <LandingPage>
-            <PreviewCard testPost={testPost} />
-          </LandingPage>
-        </App>
-      </BrowserRouter>
-    ).container
-  })
+  let container = null;
+  beforeEach(() => {
+    // setup a DOM element as a render target
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
 
-  it('Displays a title for the post', () => {
-    // console.log(JSON.stringify(container.querySelectorAll('h3').length))
-    expect(container.querySelector('h3')).toBeDefined()
-    // expect(container.querySelector('h3')).toHaveTextContent('creating a post about africa')
+  afterEach(() => {
+    // cleanup on exiting
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  describe('Preview card renders with post information', () => {
+    it("render the post title", () => {
+      act(() => {
+        render(
+          <BrowserRouter>
+            <PreviewCard post={testPost} />
+          </BrowserRouter>
+        , container)
+      })
+      expect(screen.getByText('creating a post about africa...')).toBeDefined()
+    })
+
+    it("render the post author", () => {
+      act(() => {
+        render(
+          <BrowserRouter>
+            <PreviewCard post={testPost} />
+          </BrowserRouter>
+        , container)
+      })
+      expect(screen.getByText('Author: Person12345')).toBeDefined()
+    })
+
+    it("render the post category", () => {
+      act(() => {
+        render(
+          <BrowserRouter>
+            <PreviewCard post={testPost} />
+          </BrowserRouter>
+        , container)
+      })
+      expect(screen.getByText('Contient: Africa')).toBeDefined()
+    })
+
+    it("render the date posted", () => {
+      act(() => {
+        render(
+          <BrowserRouter>
+            <PreviewCard post={testPost} />
+          </BrowserRouter>
+        , container)
+      })
+      expect(screen.getByText(`Posted: ${moment(testPost.date_posted).startOf('minute').fromNow()}`)).toBeDefined()
+    })
+
+    it("render the date posted", () => {
+      act(() => {
+        render(
+          <BrowserRouter>
+            <PreviewCard post={testPost} />
+          </BrowserRouter>
+        , container)
+      })
+      expect(screen.getByText('Zulu Nation!......')).toBeDefined()
+    })
   })
 
 })
