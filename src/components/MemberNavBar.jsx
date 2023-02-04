f7313abcc9535e92c0a68356154f93d53b9e93a9
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-02-05 08:48:59 +1100

    add gradient to navbars

--- src/components/MemberNavBar.jsx
+++ src/components/MemberNavBar.jsx
@@ -10,7 +10,7 @@ const MemberNavBar = ({ logoutMember, loggedInMember }) => {
     // display navbar for members
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient fixed-top">
           <div className="container-fluid">
               <Link className="navbar-brand ms-0 ps-0" to="/">
                 <img src={logo} width="200" height="40" alt=""></img>
