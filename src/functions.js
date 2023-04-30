50e417eef1dacd5de601457ec9861730fb1bf924
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-05-01 08:39:57 +1000

    change route to new Railway deployment of Express server

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,6 @@
 async function fetchPosts(setPosts, URI = "") {
   const result = await fetch(
-    `https://travellers-forum.up.railway.app/posts${URI}`
+    `https://ca-t3a2-b-travelers-forum-server-production.up.railway.app/posts${URI}`
   );
   const data = await result.json();
   setPosts(data);
@@ -8,7 +8,7 @@ async function fetchPosts(setPosts, URI = "") {
 
 async function fetchMember(setMemberHasRated, MemberId) {
   const result = await fetch(
-    `https://travellers-forum.up.railway.app/members/${MemberId}`
+    `https://ca-t3a2-b-travelers-forum-server-production.up.railway.app/members/${MemberId}`
   );
   const data = await result.json();
   setMemberHasRated(data.has_rated);
